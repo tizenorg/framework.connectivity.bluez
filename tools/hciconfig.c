50edfe6194335d7c8178437c161f056ea63a91db
Vikrampal <vikram.pal@samsung.com>
2014-12-04 18:29:35 +0530

    tools/hciconfig: Fix null termination after strncpy()
    The buffer 'cls_str' may not be null-terminated after the call to strncpy()
    
    Change-Id: Id1f5acb6f6cbfae32766786a7d50d9a7cc51802a

--- tools/hciconfig.c
+++ tools/hciconfig.c
@@ -67,6 +67,7 @@ static void print_dev_list(int ctl, int flags)
 
 	if (ioctl(ctl, HCIGETDEVLIST, (void *) dl) < 0) {
 		perror("Can't get device list");
+		free(dl);
 		exit(1);
 	}
 
@@ -82,6 +83,8 @@ static void print_dev_list(int ctl, int flags)
 		}
 		print_dev_info(ctl, &di);
 	}
+
+	free(dl);
 }
 
 static void print_pkt_type(struct hci_dev_info *di)
@@ -747,12 +750,15 @@ static char *get_minor_device_name(int major, int minor)
 		switch (minor & 48) {
 		case 16:
 			strncpy(cls_str, "Keyboard", sizeof(cls_str));
+			cls_str[strlen(cls_str)] = '\0';
 			break;
 		case 32:
 			strncpy(cls_str, "Pointing device", sizeof(cls_str));
+			cls_str[strlen(cls_str)] = '\0';
 			break;
 		case 48:
 			strncpy(cls_str, "Combo keyboard/pointing device", sizeof(cls_str));
+			cls_str[strlen(cls_str)] = '\0';
 			break;
 		}
 		if ((minor & 15) && (strlen(cls_str) > 0))
